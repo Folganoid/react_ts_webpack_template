d606c07f5ee2c6e4f604fde4c401b4edc2995ea4
Andrii Holubkov <folganoid@yahoo.com>
2023-04-21 18:16:10 +0300

    init: css and images

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import './styles.css';
+import img from './img.png';
+import imgSvg from './git.svg';
 
 export default function App() {
-  return <h1>TEST !!!</h1>;
+  return (
+    <>
+      <h1>TEST !!!</h1>
+      <img src={img} alt="image" />
+      <img src={imgSvg} alt="git logo" />
+    </>
+  );
 }
